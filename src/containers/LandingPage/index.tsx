78e72178f6e6563cc1a037cd8e9efb0ce9fb0847
Jana Grimm <jana.grimm@triply.cc>
2022-11-15 16:43:00 +0100

    ADDED example button and state

--- src/containers/LandingPage/index.tsx
+++ src/containers/LandingPage/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import Svg from "components/Svg";
 import triplyLogo from "./triply.svg";
 import styles from "./style.scss";
-import { Container } from "@material-ui/core";
+import { Button, Container } from "@material-ui/core";
 interface Props {}
 const LandingPage: React.FC<Props> = () => {
+  const [someObject, setSomeObject] = React.useState<{ key: number }>();
   return (
     <Container component="article">
       <h2>Landing Page</h2>
+      <Button
+        onClick={() => {
+          setSomeObject({ key: 1 });
+        }}
+        variant="contained"
+      >
+        Contained
+      </Button>
+      <div>Result: {JSON.stringify(someObject, null, 2)}</div>
       <span>
         Brought to you by <Svg className={styles.svg} src={triplyLogo} />
       </span>
